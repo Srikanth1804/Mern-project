dbd5358ac8d21eb34e6e1a121e3bc04d9cdad51b
Srikanth <mrmadukkur1804@gmail.com>
2024-06-19 22:10:55 +0530

    .

--- E-commerce/Collection.js
+++ E-commerce/Collection.js
@@ -18,3 +18,13 @@ for(count=0;count<productlist.length;count++){
 
 
 })
+
+let togglebar=document.querySelector(".navbar-toggle-bar")
+let slidebar=document.querySelector(".slide-bar")
+togglebar.addEventListener("click",function(){
+        slidebar.style.left="0"
+})
+let closebtn=document.getElementById("closebtn")
+closebtn.addEventListener("click",function(){
+    slidebar.style.left="-50%"
+})
