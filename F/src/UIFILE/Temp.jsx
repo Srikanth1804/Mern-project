31de19e4c5adf9ff52d97f9afd3d8ebad9998405
Srikanth <mrmadukkur1804@gmail.com>
2024-06-28 17:11:38 +0530

    .

---
+++ F/src/UIFILE/Temp.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Bodysection from './Bodysection'
+import Middlesection from './Middlesection'
+import Tagsection from './Tagsection'
+import Footersection from './Footersection'
+import Footerbar from './Footerbar'
+
+const Temp = () => {
+  return (
+    <div id='RWD'>
+  <Navbar/>
+  <Bodysection/>
+  <Middlesection/>
+  <Tagsection/>
+  <Footersection/>
+  <Footerbar/>
+    </div>
+  )
+}
+
+export default Temp
